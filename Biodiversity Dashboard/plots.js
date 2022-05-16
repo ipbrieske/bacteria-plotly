f3da36e94676baedbe5ab02bebf2ae2ca70297ae
Isaac Brieske <ipbrieske@gmail.com>
2022-05-16 11:29:09 -0700

    built biodiversity dashboard

---
+++ Biodiversity Dashboard/plots.js
@@ -0,0 +1,39 @@
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      //console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+  function optionChanged(newSample) {
+    console.log(newSample);
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  };
+
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+  
+        var variables = ['ID', 'ETHNICITY', 'GENDER', 'AGE', 'LOCATION', 'BBTYPE', 'WFREQ'];
+
+        PANEL.html("");
+
+        for (var i = 0; i < variables.length; i++) {
+            var variable = variables[i].toLowerCase(); 
+            PANEL.append("h6").text(variables[i] + ': ' + result[variable]);
+        };
+    });
+  };
